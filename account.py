c1eaac5edb687718a7b84eb7351d45551fbe45c0
Steven <steven.samuel@ui.ac.id>
2024-06-08 08:09:54 +0700

    tahap1

---
+++ account.py
@@ -0,0 +1,127 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, auth
+import json
+import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+firebase_credentials = os.getenv('FIREBASE_CREDENTIALS_JSON')
+
+cred = credentials.Certificate(firebase_credentials)
+firebase_admin.initialize_app(cred)
+
+def app():
+    st.title('Welcome to :blue[UMKMAI]')
+
+    if 'username' not in st.session_state:
+        st.session_state.username = ''
+    if 'useremail' not in st.session_state:
+        st.session_state.useremail = ''
+
+    def sign_up_with_email_and_password(email, password, username=None, return_secure_token=True):
+        try:
+            rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp"
+            payload = {
+                "email": email,
+                "password": password,
+                "returnSecureToken": return_secure_token
+            }
+            if username:
+                payload["displayName"] = username
+            payload = json.dumps(payload)
+            r = requests.post(rest_api_url, params={"key": os.getenv('FIREBASE_API_KEY')}, data=payload)
+            return r.json().get('email', '')
+        except Exception as e:
+            st.warning(f'Signup failed: {e}')
+
+    def sign_in_with_email_and_password(email=None, password=None, return_secure_token=True):
+        rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
+        try:
+            payload = {
+                "returnSecureToken": return_secure_token
+            }
+            if email:
+                payload["email"] = email
+            if password:
+                payload["password"] = password
+            payload = json.dumps(payload)
+            r = requests.post(rest_api_url, params={"key": os.getenv('FIREBASE_API_KEY')}, data=payload)
+            data = r.json()
+            return {
+                'email': data['email'],
+                'username': data.get('displayName')
+            }
+        except Exception as e:
+            st.warning(f'Signin failed: {e}')
+
+    def reset_password(email):
+        try:
+            rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode"
+            payload = {
+                "email": email,
+                "requestType": "PASSWORD_RESET"
+            }
+            payload = json.dumps(payload)
+            r = requests.post(rest_api_url, params={"key": os.getenv('FIREBASE_API_KEY')}, data=payload)
+            if r.status_code == 200:
+                return True, "Reset email Sent"
+            else:
+                return False, r.json().get('error', {}).get('message')
+        except Exception as e:
+            return False, str(e)
+
+    def login():
+        try:
+            userinfo = sign_in_with_email_and_password(st.session_state.email_input, st.session_state.password_input)
+            st.session_state.username = userinfo['username']
+            st.session_state.useremail = userinfo['email']
+            st.session_state.signedout = True
+            st.session_state.signout = True
+        except:
+            st.warning('Login Failed')
+
+    def logout():
+        st.session_state.signout = False
+        st.session_state.signedout = False
+        st.session_state.username = ''
+        st.session_state.useremail = ''
+
+    def forget():
+        email = st.text_input('Email')
+        if st.button('Send Reset Link'):
+            success, message = reset_password(email)
+            if success:
+                st.success("Password reset email sent successfully.")
+            else:
+                st.warning(f"Password reset failed: {message}")
+
+    if "signedout" not in st.session_state:
+        st.session_state["signedout"] = False
+    if 'signout' not in st.session_state:
+        st.session_state['signout'] = False
+
+    if not st.session_state["signedout"]:
+        choice = st.selectbox('Login/Signup', ['Login', 'Sign up'])
+        email = st.text_input('Email Address')
+        password = st.text_input('Password', type='password')
+        st.session_state.email_input = email
+        st.session_state.password_input = password
+
+        if choice == 'Sign up':
+            username = st.text_input("Enter your unique username")
+            if st.button('Create my account'):
+                sign_up_with_email_and_password(email=email, password=password, username=username)
+                st.success('Account created successfully! Please log in using your email and password.')
+        else:
+            st.button('Login', on_click=login)
+            st.subheader('Lupa Password')
+            forget()
+
+    if st.session_state.signout:
+        st.text('Name: ' + st.session_state.username)
+        st.text('Email id: ' + st.session_state.useremail)
+        st.button('Sign out', on_click=logout)
